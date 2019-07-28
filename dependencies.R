7c33751dc79e2980ea5542e89a58c9441e608a62
kavya Ankush <kavyankush25@gmail.com>
2019-07-29 00:55:50 +0530

    Add files via upload

---
+++ dependencies.R
@@ -0,0 +1,22 @@
+#Kavya Ankush-11915080
+#Madhuri Chinta - 11915055
+#Siddartha Tallapragada- 11915026
+
+if (!require(shiny)){install.packages("shiny")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(stringr)){install.packages("stringr")
+if (!require(textrank)){install.packages("textrank")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+library(shiny)
+library(udpipe)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+library(dplyr)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
