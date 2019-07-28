e35e0b835e38ad95173f3c90b318d57dd8c51fdc
kavya Ankush <kavyankush25@gmail.com>
2019-07-28 23:23:23 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,87 @@
+shinyServer(function(input, output) {
+#Reading The input file
+  D1 <- reactive({
+    if (is.null(input$new)) {return(NULL) } 
+    else{
+      Data <- readLines(input$new$datapath)
+    #  Data  =  str_replace_all(Data, "<.*?>", "")
+     # Data = Data[Data!= ""]
+      #str(Data)
+      return(Data)
+    }
+  })
+#Loading Model file
+  model = reactive({
+    model = udpipe_load_model("english-ewt-ud-2.3-181115.udpipe")  # file_model only needed
+    return(model)
+  })
+#  
+  annotate = reactive({
+    a <- udpipe_annotate(model(),x = D1())
+    a <- select(as.data.frame(a),-sentence)
+    head(a,100)
+  })
+  
+  annotate1=reactive({
+    b <- udpipe_annotate(model(),x = D1())
+    b <- select(as.data.frame(b),-sentence)
+    return(b)
+  })
+  
+#download output file  
+  output$downloadfile <- downloadHandler(
+    file = function(){
+      "data.csv"
+    },
+    content = function(new){
+      write.csv(annotate1(),new,row.names = FALSE)
+    }
+  )
+#render the annotated table
+  output$table = renderDataTable({
+      out = annotate()
+      return(out)
+  })
+#wordcloud-noun
+  output$Nouns = renderPlot({
+    if(is.null(input$new)){return(NULL)}
+    else{
+      all_nouns = annotate1() %>% subset(., upos %in% "NOUN") 
+      top_nouns = txt_freq(all_nouns$lemma)  # txt_freq() calcs noun freqs in desc order
+      
+      wordcloud(top_nouns$key,top_nouns$freq, min.freq = 3,colors = 1:10 )
+    }
+  })
+#wordcloud-Verb  
+  output$Verbs = renderPlot({
+    if(is.null(input$new)){return(NULL)}
+    else{
+      all_verbs = annotate1() %>% subset(., upos %in% "VERB") 
+      top_verbs = txt_freq(all_verbs$lemma)
+      
+      wordcloud(top_verbs$key,top_verbs$freq, min.freq = 3,colors = 1:10 )
+    }
+  })
+#co-occurence
+  output$coplot = renderPlot({
+    if(is.null(input$new)){return(NULL)}
+    else{
+      occ <- cooccurrence(x = subset(annotate(), upos %in% input$upos),
+        term = "lemma",
+        group = c("doc_id", "paragraph_id", "sentence_id"))
+      
+      network <- head(occ, 50)
+      network <- igraph::graph_from_data_frame(network)
+      
+      ggraph(network, layout = "fr") +  
+        
+        geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "orange") +  
+        geom_node_text(aes(label = name), col = "darkgreen", size = 6) +
+        
+        theme_graph(base_family = "Arial Narrow") +  
+        theme(legend.position = "none") +
+        
+        labs(title = "Cooccurrences within 3 words distance", subtitle = "Select the check boxes in the left pane")
+    }
+  })
+})
---
+++ ui.R
@@ -0,0 +1,30 @@
+setwd('C:/Users/kavya/Documents/newdir')
+Ui<-shinyUI( 
+  fluidPage(
+    titlePanel("NLP"),
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("new", "text file here"),
+        checkboxGroupInput("upos", label = h3("Upos selection"), choices = list("Adjective" = "ADJ", "Noun" = "NOUN" , "Proper Noun" = "PROPN", "Adverb" = "ADV", "Verb" = "VERB"), selected = c('ADJ', 'NOUN', 'PROPN'))),
+
+      mainPanel(tabsetPanel(type = "tabs",
+                tabPanel("Description", br(), 
+                         p('This app performs standard NLP functionalities on the text file.', align="justify"),
+                         br(),
+                         p("Standard NLP is performed on the text file using udpipe in R and the document is displayed in the annotataed document tab.", align="justify"),
+                         p("Two wordclouds will be displayed in the next tab with all the frequently used nous and  frquently used verbs."),
+                         p("The last tab displays top-30 co-occurences."),
+                         br(),
+                h3(p('To Use this App')),
+                p("Click on browse button in 'text file here' field and upload a text file.", align="justify")),
+                tabPanel("Annotated document",dataTableOutput('table'),
+                         downloadButton("downloadfile", "Download Annotated Data")),
+                tabPanel("Wordclouds", h2("NounCloud"),
+                         plotOutput('Nouns', height = 300, width = 300),
+                         h2("VerbCloud"),
+                         plotOutput('Verbs', height = 300, width = 300)),
+                tabPanel("Co-occurences",plotOutput('coplot'))
+                )
+      )
+  )
+))
